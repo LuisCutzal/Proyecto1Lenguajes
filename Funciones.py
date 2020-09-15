0b05a15589281635795b483882e367534db23956
LuisCutzal <2842258270404@ingenieria.usac.edu.gt>
2020-09-14 19:44:13 -0600

    funcionan las transiciones
    no funciona bien los estados, al memento de imprimir me sale toda una lista con todos los objetos en ellas, el cual no tiene que ser asi

--- Funciones.py
+++ Funciones.py
@@ -5,8 +5,7 @@ global automata
 automata=list()
 global aceptacion
 aceptacion=list()
-global tra
-tra = list()
+
 def Agregar():
     #input("hola")
     #automata= Automatas.Automatas()
@@ -17,15 +16,13 @@ def Agregar():
         aceptacion.append(list(estados))
         si=input("¿desea ingresar otro estado?, [Y/N]: ")
         if si=="Y" or si=="y" or si=="Yes":
-            #print(aceptacion)
             ingreso=True
         else:
             ingreso=False
-    #print(aceptacion)
     Alfabeto=input("Ingrese el Alfabeto del AFD separados por comas: ").split(',')
     EstadoInicial=input("Ingrese el Estado Inicial del AFD: ")
-    EstadosAceptacion=input("Ingrese los Estados de Aceptacion del AFD separados por comas: ").split(',')
-    Transicion = input("Ingrese las Transiciones del AFD: ").split(',')
+    EstadosAceptacion=input("Ingrese los Estados de Aceptacion del AFD separados por comas: ").split(',') #devuelve una lista
+    Transicion = input("Ingrese las Transiciones del AFD: ").split('; |, ')
     automata.append(Automatas.Automatas(nom,aceptacion,Alfabeto,EstadoInicial,EstadosAceptacion,Transicion))
     print("[", nom , "," , aceptacion , ",", Alfabeto , ",", EstadoInicial , ",", EstadosAceptacion , ",", Transicion , "]")
     input("hola: ")
